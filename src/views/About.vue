045e25a8b91542f6123cbf02e78b204ca2dd6f90
Pinny Geliebter <pgibralter@gmail.com>
2022-01-07 09:12:12 -0500

    adds timeline and store tests to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Navbar></Navbar>
+
+    <Timeline :timeline="this.$store.state.timeline"></Timeline>
+    <button @click="logCount()" class="btn btn-dark">Log the count</button>
+    <button @click="increaseCount()" class="btn btn-dark">
+      Increase the count
+    </button>
   </div>
 </template>
+<script>
+import Navbar from '../components/Navbar';
+import Timeline from '../components/Timeline';
+
+export default {
+  components: {
+    Navbar: Navbar,
+    Timeline: Timeline,
+  },
+  methods: {
+    logCount: function () {
+      console.log(this.$store.state.count);
+    },
+    increaseCount: function () {
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
