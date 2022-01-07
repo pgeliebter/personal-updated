8a5d7816100e1b78bb544f760a1be9a6e3934bc2
Pinny Geliebter <pgibralter@gmail.com>
2022-01-07 09:11:28 -0500

    wip timeline not showing on page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,133 +8,8 @@
 
     <div data-scroll-container>
       <!--Header Start-->
-      <header class="header-fixed-top header-transparent z-index-fixed">
-        <nav class="navbar navbar-expand-lg navbar-dark py-3">
-          <div class="container position-relative">
-            <a class="navbar-brand width-30" href="index.html">
-              <img src="assets/img/logo/logo-a.svg" alt class="img-fluid" />
-            </a>
 
-            <div
-              class="d-flex align-items-center navbar-no-collapse-items order-lg-last"
-            >
-              <div class="nav-item me-3 me-lg-0">
-                <a href="#" class="btn btn-dark btn-sm">Purchase</a>
-              </div>
-              <div class="nav-item me-3 me-lg-0">
-                <a
-                  data-bs-toggle="offcanvas"
-                  href="#offcanvasEnd"
-                  class="nav-link text-dark ms-3"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="currentColor"
-                    class="bi bi-list"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
-
-      <!--Vertical header offcanvas-->
-      <!--Offcanvas dark-->
-      <div
-        class="offcanvas offcanvas-end bg-shade-primary text-white"
-        tabindex="-1"
-        id="offcanvasEnd"
-        aria-labelledby="offcanvasEnd"
-      >
-        <div class="border-bottom offcanvas-header">
-          <button
-            type="button"
-            class="btn-close text-reset p-0 m-0 size-40 center-both ms-auto"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid meet"
-              width="24"
-              height="24"
-              viewBox="0 0 128 128"
-            >
-              <g>
-                <path
-                  stroke="currentColor"
-                  stroke-width="8"
-                  stroke-linecap="square"
-                  fill="none"
-                  d="M7 7l114 114m0-114l-114 114"
-                />
-              </g>
-            </svg>
-          </button>
-        </div>
-        <div class="offcanvas-body p-4 px-xl-5">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a href="#" class="nav-link fs-2 fw-normal">Index</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link fs-2 fw-normal">About</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link fs-2 fw-normal">Portfolio</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link fs-2 fw-normal">Capabilities</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link fs-2 fw-normal">Get in touch</a>
-            </li>
-          </ul>
-        </div>
-        <div class="offcanvas-footer p-4 px-xl-5">
-          <ul class="list-unstyled mb-0">
-            <li class="pb-4">
-              <small class="text-muted d-block">Email us:</small>
-              <a href="mailto:mail@domain.com" class="link-underline fs-5"
-                >mail@domain.com</a
-              >
-            </li>
-            <li>
-              <ul class="list-inline">
-                <li class="list-inline-item me-3">
-                  <a href="#" class="fs-5 text-muted">
-                    <i class="bi bi-facebook"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item me-3">
-                  <a href="#" class="fs-5 text-muted">
-                    <i class="bi bi-twitter"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item me-3">
-                  <a href="#" class="fs-5 text-muted">
-                    <i class="bi bi-linkedin"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#" class="fs-5 text-muted">
-                    <i class="bi bi-instagram"></i>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Navbar></Navbar>
       <!--Main content-->
       <main>
         <!--page-hero-->
@@ -185,14 +60,14 @@
                 <div class="d-inline-flex">
                   <ul class="js-hover-img">
                     <li class="js-hover-img-item">
-                      <h1 class="js-hover-img-link display-1 mb-0">
+                      <h1 class="js-hover-img-link display-3 mb-0">
                         <small class="fs-4 ls-1 fw-normal"
-                          >👋Hi, I Am Aaron Nunez</small
+                          >👋 Hi, I Am Pinny Geliebter</small
                         >
-                        <br />Freelance Web Designer and Visual Developer.
+                        <br />A Software And <br />Business Developer
                       </h1>
                       <img
-                        src="assets/img/team/3.jpg"
+                        src="/assets/img/team/3.jpg"
                         alt="Image"
                         class="img height-300 w-auto"
                       />
@@ -236,6 +111,7 @@
                 </h2>
               </div>
             </div>
+
             <div class="row justify-content-center">
               <!--Feature column-->
               <div class="col-12 col-md-4 mb-5 mb-md-0">
@@ -334,9 +210,39 @@
                   data-scroll-direction="vertical"
                   data-scroll-speed="1"
                 >
-                  My creative journey started at Jaipur where I studied for the
-                  past years. I’ve been able to work with talented people,
-                  especially across the globe.
+                  My current journey started at Comprehensive where I worked as
+                  an Ops Analyst for 3 years. I’ve had the privalge to work on
+                  many different project there
+                </h3>
+              </div>
+              <div class="col-md-6 order-1 mb-5 mb-md-0 position-relative">
+                <div
+                  class="overflow-hidden position-relative"
+                  data-scroll
+                  data-scroll-direction="vertical"
+                  data-scroll-speed="3"
+                >
+                  <img
+                    src="assets/img/projects/lg4.jpg"
+                    alt
+                    class="img-fluid flip-x d-block"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container py-7 py-lg-12 position-relative z-index-1">
+            <div class="row align-items-stretch">
+              <div class="col-md-10 col-xl-5 ms-auto order-last">
+                <h3
+                  class="mb-5 display-6 fw-normal position-relative z-index-1"
+                  data-scroll
+                  data-scroll-direction="vertical"
+                  data-scroll-speed="1"
+                >
+                  My current journey started at Comprehensive where I worked as
+                  an Ops Analyst for 3 years. I’ve had the privalge to work on
+                  many different project there
                 </h3>
               </div>
               <div class="col-md-6 order-1 mb-5 mb-md-0 position-relative">
@@ -716,10 +622,13 @@
 
 <script>
 import LocomotiveScroll from 'locomotive-scroll';
+import Navbar from '../components/Navbar.vue';
+import Timeline from '../components/Timeline';
+
 export default {
-  name: 'locoScroll',
-  props: {
-    msg: String,
+  components: {
+    Navbar: Navbar,
+    Timeline: Timeline,
   },
   data() {
     return {
