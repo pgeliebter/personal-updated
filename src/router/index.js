9444e1697d1ea0178fa2eb139af7f2c9e63e0e83
Pinny Geliebter <pgibralter@gmail.com>
2022-01-10 18:01:44 -0500

    adds id as param for project show page

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
-    path: '/project',
+    path: '/project/:id',
     name: 'Project',
     component: Project,
   },
